a11d7a607f1c1d9b2d6bfaa4486ab945a78aab90
Vincent Vallet <vincentvallet@voodoo.io>
2019-12-19 11:49:51 +0100

    Add tests.

---
+++ tests/reloader.test.js
@@ -0,0 +1,38 @@
+const Reloader = require('../index')
+
+describe('Realoader', () => {
+    describe('get()', () => {
+        afterEach(() => {
+            jest.clearAllMocks()
+        })
+
+        it('should launch interval to retrieve config', async () => {
+            const reloader = new Reloader(100)
+            const mockRetrieveConf = jest.spyOn(reloader, 'retrieve')
+            let counter = 0
+            reloader.addFunction('test', () => { return new Promise(resolve => resolve(++counter)) })
+            await reloader.init()
+
+            let res = await reloader.get('test')
+            expect(res).toEqual(1)
+            expect(mockRetrieveConf.mock.calls.length).toEqual(1)
+
+            await new Promise(resolve => setTimeout(resolve, 100))
+            res = await reloader.get('test')
+            expect(res).toEqual(1)
+            expect(mockRetrieveConf.mock.calls.length).toEqual(2)
+
+            await new Promise(resolve => setTimeout(resolve, 100))
+
+            expect(mockRetrieveConf.mock.calls.length).toEqual(3)
+            res = await reloader.get('test')
+            expect(res).toEqual(2)
+
+            const secondInit = await reloader.init()
+            expect(secondInit.test).toEqual(2)
+
+            const all = reloader.getAll()
+            expect(all.test).toEqual(2)
+        })
+    })
+})
